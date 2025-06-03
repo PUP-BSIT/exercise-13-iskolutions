bdc40b12359a676fc0ad54120ad2546cc94c885e
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-06-03 23:46:11 +0800

    implement loop with display_section() helper

--- iskolutions/franco.py
+++ iskolutions/franco.py
@@ -11,3 +11,30 @@ def print_border():
 def return_to_main_menu():
     print("Press Enter to return to the main menu...")
     input()
+
+def display_section(func):
+    clear_screen()
+    func()
+
+def pearl():
+    menu_options = {}
+
+    while True:
+        clear_screen()
+        print("Hello there, I am Fernette Pearl Franco!")
+        print_border()
+        print("8. Exit")
+        print_border()
+
+        choice = input("Please select an option (1-8): ")
+
+        if choice == '8':
+            print("Thank you for visiting! Goodbye!")
+            break
+        elif choice in menu_options:
+            display_section(menu_options[choice])
+        else:
+            print("Invalid choice. Please select a number between 1 and 8.")
+            return_to_main_menu()
+
+pearl()
