70bb4896bb849b9cfb39f160b818533588062ad5
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-06-03 21:15:42 +0800

    add choice processing in menu

--- iskolutions/causon.py
+++ iskolutions/causon.py
@@ -21,7 +21,27 @@ def display_get_choice():
         print("Invalid input! Please enter a number.")
         return UNSET_OPTION
 
+def process_choice(choice):
+    match choice:
+        case 1:
+            # TODO: display basic info
+            pass
+        case 2:
+            # TODO: display goals
+            pass
+        case 3:
+            # TODO: implement password checker
+            pass
+        case 4:
+            system("cls")
+        case _:
+            print("Invalid choice! Please select a valid option.")
+            input("Press Enter to continue...")
+            system("cls")
+            pass
+
 def miko():
     choice = UNSET_OPTION
     while choice != EXIT_OPTION:
         choice = display_get_choice()
+        process_choice(choice)
