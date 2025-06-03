5c1f92cf9b8ad84f0fdbb2be8844a2822fc2be10
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-06-03 22:00:34 +0800

    import and call causon on main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from iskolutions import causon
 from os import system
 
 UNSET_OPTION = -1
@@ -5,7 +6,7 @@ EXIT_OPTION = 6
 
 def display_get_choice():
     print("============Select an Option============")
-    print("1. ")
+    print("1. Miko Lorenz O. Causon")
     print("2. ")
     print("3. ")
     print("4. ")
@@ -25,8 +26,7 @@ def display_get_choice():
 def process_choice(choice):
     match choice:
         case 1:
-            # TODO (Causon): replace your function call here
-            pass
+            causon.miko()
         case 2:
             # TODO (Efondo): replace your function call here
             pass
