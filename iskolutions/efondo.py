b5d3923a73a703467c131720215166146526f712
efondo_aaronkyle <akyleefondo@gmail.com>
2025-06-03 23:29:42 +0800

    add display basic information function

--- iskolutions/efondo.py
+++ iskolutions/efondo.py
@@ -1,1 +1,10 @@
 from os import system
+
+def display_basic_information():
+    system("cls")
+    print("Name: Aaron Kyle D. Efondo")
+    print("Gender: Male")
+    print("Birthday: June 7, 2004")
+    print("Civil Status: Single")
+    print("Nationality: Filipino")
+    print("Hobbies: Playing volleyball, coding, & watching movies")
