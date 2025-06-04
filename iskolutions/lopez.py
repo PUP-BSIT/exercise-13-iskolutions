5d6ea60e34274f2830730c74848721c5c62b4931
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-06-04 08:03:42 +0800

    add get_weather_information function in lopez.py

--- iskolutions/lopez.py
+++ iskolutions/lopez.py
@@ -8,7 +8,7 @@ def display_get_choice():
     print("=== WELCOME TO JAKIM LOPEZ'S PROFILE ===")
     print("1. Get to know me better")
     print("2. Goals in life")
-    print("3. ") # TODO: add feature
+    print("3. Weather Update")
     print("4. Exit")
 
     try:
@@ -25,7 +25,7 @@ def process_choice(choice):
         case 2:
             display_goals()
         case 3:
-            pass # TODO: replace function call
+            get_weather_information()
         case 4:
             pass # TODO: replace function call
         case _:
@@ -52,6 +52,41 @@ def display_goals():
           + " external-facing opportunities")
     print("-To pass INTEG 202 course this 2024-2025 school year")
     print("\nPress any key to continue...")
+
+def get_weather_information():
+    print("=== WEATHER UPDATE ===")
+    input_city = input("Enter any city around the world that "
+                       + "you want to check the weather "
+                       + "for (e.g. Taguig): ").strip()
+
+    if not input_city:
+        print("\nInvalid input! Enter a valid city name.")
+        return {}
+
+    weather_data = {}
+
+    # API key from OpenWeatherMap (rate-limited upto 60 calls/minute)
+    url = ('http://api.openweathermap.org/data/2.5/weather?q'
+           + f'={input_city}&appid=aacae991094d7dae63e3a2b8bba7872c'
+           + '&units=metric')
+    
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['main']['temp']
+        weather = data['weather'][0]['description']
+        weather_data[input_city] = {'temperature': temperature, 
+                              'weather': weather}
+        print(f"\n{input_city}: {temperature}°C, {weather}")
+    else:
+        print(f"\nError retrieving weather data for {input_city}. "
+              + "Either it is an (1) invalid city or (2) check your "
+              + "internet connection and try again.")
+        
+    print("\nPress any key to continue...")
+    input()
+    return weather_data
 
 def jakim():
     choice = UNSET_OPTION
